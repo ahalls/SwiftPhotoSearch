4e66b12b0b180b78530a7a90d3b66a230a8d5ab5
Andrew Halls <andrew@galtsoft.com>
2014-08-14 10:54:12 +0800

    Added Instagram Call for /IOS-002/unit/4/lesson/3/assignment/1

--- PhotoSearch/PhotoSearch/ViewController.swift
+++ PhotoSearch/PhotoSearch/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    let manager = AFHTTPRequestOperationManager()
+
+    manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=d782212a48f44653bea97452dd87da2b",
+      parameters: nil,
+      success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+        println("JSON: " + responseObject.description)
+      },
+      failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+        println("Error: " + error.localizedDescription)
+    })
+
+
   }
 
   override func didReceiveMemoryWarning() {
