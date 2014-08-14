cea35efd70bf2c89455db7daa41bf5d374eb9ad3
Andrew Halls <andrew@galtsoft.com>
2014-08-14 11:34:21 +0800

    Added Searchbar /IOS-002/unit/4/lesson/3/assignment/7

--- PhotoSearch/PhotoSearch/ViewController.swift
+++ PhotoSearch/PhotoSearch/ViewController.swift
@@ -8,44 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
-  @IBOutlet weak var searchBar: UISearchBar!
-  @IBOutlet weak var scrollView: UIScrollView!
+    class ViewController: UIViewController, UISearchBarDelegate {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+      @IBOutlet weak var scrollView: UIScrollView!
 
-    let manager = AFHTTPRequestOperationManager()
+      let instagramClientID = "d782212a48f44653bea97452dd87da2b"
 
-    manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=d782212a48f44653bea97452dd87da2b",
-      parameters: nil,
-      success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-        self.loadData(responseObject)
-        //println("JSON: " + responseObject.description)
-      },
-      failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
-        println("Error: " + error.localizedDescription)
-    })
-
-
-  }
-  func loadData(responseObject: AnyObject!) {
-    if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
-      self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
-      for var i = 0; i < dataArray.count; i++ {
-        let dataObject: AnyObject = dataArray[i]
-        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
-          println("image " + String(i) + " URL is " + imageURLString)
-          let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString))
-          let imageView = UIImageView(image: UIImage(data: imageData))
-          imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
-          self.scrollView.addSubview(imageView)
+      func searchInstagramByHashtag(searchString: String) {
+        for subview in self.scrollView.subviews {
+          subview.removeFromSuperview()
         }
-      }
-    }
-  }
-}
 
+        let instagramURLString = "https://api.instagram.com/v1/tags/" + searchString + "/media/recent?client_id=" + instagramClientID
+
+        let manager = AFHTTPRequestOperationManager()
+
+        manager.GET( instagramURLString,
+          parameters: nil,
+          success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+            println("JSON: " + responseObject.description)
+
+            if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
+              self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
+              for var i = 0; i < dataArray.count; i++ {
+                let dataObject: AnyObject = dataArray[i]
+                if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                  println("image " + String(i) + " URL is " + imageURLString)
+
+                  let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString))
+                  let imageView = UIImageView(image: UIImage(data: imageData))
+                  imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
+                  self.scrollView.addSubview(imageView)
+                }
+              }
+            }
+          },
+          failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+            println("Error: " + error.localizedDescription)
+        })
+      }
+
+      func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
+
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
+      }
+
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        searchInstagramByHashtag("clararockmore")
+      }
+      override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+      }
+}
 
