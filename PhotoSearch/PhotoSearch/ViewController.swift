fb08d4bc80ddf4ca3a73b4ae7d16116a85fc57ff
Andrew Halls <andrew@galtsoft.com>
2014-08-14 11:16:51 +0800

    Display Data on Scroll View /IOS-002/unit/4/lesson/3/assignment/5

--- PhotoSearch/PhotoSearch/ViewController.swift
+++ PhotoSearch/PhotoSearch/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var scrollView: UIScrollView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,8 @@ class ViewController: UIViewController {
     manager.GET( "https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=d782212a48f44653bea97452dd87da2b",
       parameters: nil,
       success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
-        println("JSON: " + responseObject.description)
+        self.loadData(responseObject)
+        //println("JSON: " + responseObject.description)
       },
       failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
         println("Error: " + error.localizedDescription)
@@ -27,12 +31,21 @@ class ViewController: UIViewController {
 
 
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func loadData(responseObject: AnyObject!) {
+    if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
+      self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
+      for var i = 0; i < dataArray.count; i++ {
+        let dataObject: AnyObject = dataArray[i]
+        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+          println("image " + String(i) + " URL is " + imageURLString)
+          let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString))
+          let imageView = UIImageView(image: UIImage(data: imageData))
+          imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
+          self.scrollView.addSubview(imageView)
+        }
+      }
+    }
   }
-
-
 }
+
 
